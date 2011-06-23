4c7c312e0dac1eb99b0dbdb27850699d264a54b8
hector <hector1618@gmail.com>
2011-06-23 18:35:20 +0530

    Fibonacci function (iterative) by Hector

--- fibiter.py
+++ fibiter.py
@@ -2,6 +2,7 @@
 
 # Function for Fibonacci by Hector
 
+p = 1000000007
 f0, f1 = 0, 1
 
 while True:
@@ -14,5 +15,7 @@ while True:
     for i in range (2,n+1):
         b = a + b
         a = b - a
+        b = b%p
+        a = a%p
     print "fib(%d) = %d \n" %(n, b)
     break
